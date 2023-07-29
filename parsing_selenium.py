056a83141bb9688c151aabce04df9c9bd07cc8a5
Тутункин Владислав <vladislav-login94@yandex.ru>
2023-07-30 01:14:53 +0300

    Add work with postgresql

--- parsing_selenium.py
+++ parsing_selenium.py
@@ -1,4 +1,5 @@
 import time
+import psycopg2
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -13,7 +14,24 @@ soup = BeautifulSoup(html, 'html.parser')
 prices = soup.find_all('strong', class_='styles-module-root-LIAav')
 apartments = soup.find_all('h3', class_='styles-module-root-TWVKW styles-module-root-_KFFt styles-module-size_l-_oGDF styles-module-size_l-hruVE styles-module-ellipsis-LKWy3 styles-module-weight_bold-Kpd5F stylesMarningNormal-module-root-OSCNq stylesMarningNormal-module-header-l-qvNIS')
 
-for apartment, price in zip(apartments, prices):
-    m = apartment.get_text()
-    p = price.get_text()
-    print(f'{m}, цена - {p}')
+conn = psycopg2.connect(dbname='parsing', user='postgres', password='postgres', host='127.0.0.1', port='5432')
+conn.autocommit = True
+
+with conn:
+    with conn.cursor() as cursor:
+        print('Подключение установлено')
+        apartment_list = []
+        price_list = []
+        for apartment, price in zip(apartments, prices):
+            m = apartment.get_text()
+            p = price.get_text()
+            apartment_list.append(m)
+            price_list.append(p)
+        data = zip(apartment_list, price_list)
+        data_list = list(data)
+
+        cursor.executemany("INSERT INTO flats (title, price) VALUES (%s, %s)", data_list)
+        print('Данные добавлены')
+
+print(cursor.closed)
+conn.close()
