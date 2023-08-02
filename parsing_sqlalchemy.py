495b4bccb1f9b933fc6fe95d5a5abe9610f46d57
Тутункин Владислав <vladislav-login94@yandex.ru>
2023-08-02 17:16:16 +0300

    Edit parsing_sqlalchemy.py

--- parsing_sqlalchemy.py
+++ parsing_sqlalchemy.py
@@ -1,20 +1,44 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import DeclarativeBase
+import time
 
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Session
+
+driver = webdriver.Edge()
+url = 'https://www.avito.ru/moskva/kvartiry/sdam/na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg'
+driver.get(url)
+driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+time.sleep(5)
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+prices = soup.find_all('strong', class_='styles-module-root-LIAav')
+apartments = soup.find_all('h3',
+                           class_='styles-module-root-TWVKW styles-module-root-_KFFt styles-module-size_l-_oGDF styles-module-size_l-hruVE styles-module-ellipsis-LKWy3 styles-module-weight_bold-Kpd5F stylesMarningNormal-module-root-OSCNq stylesMarningNormal-module-header-l-qvNIS')
 
 engine = create_engine('sqlite:///apartments.db')
+
 
 class Base(DeclarativeBase):
     pass
 
 
-class Person(Base):
-    __tablename__ = 'people'
+class Flat(Base):
+    __tablename__ = 'flats'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    age = Column(Integer)
+    title = Column(String)
+    price = Column(String)
+
 
 Base.metadata.create_all(bind=engine)
 
-print("База данных и таблица созданы")
+with Session(autoflush=False, bind=engine) as db:
+    for apartment, price in zip(apartments, prices):
+        m = apartment.get_text()
+        p = price.get_text()
+        flat = Flat(title=m, price=p)
+        db.add(flat)
+
+    db.commit()
+    print('Данные добавлены')
